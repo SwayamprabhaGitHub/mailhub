d63ef75a94e6d096beaa237bcf89e988947acf7c
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-10-19 21:41:18 +0530

    Added the sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/shared/Navbar";
+import Sidebar from "./components/Sidebar";
 
 
 function App() {
   return (
     <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <Navbar />
+      <Sidebar />
     </div>
       
     
