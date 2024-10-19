9e4cc295721233414735cecfb9ab6ab618ce41df
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-10-19 17:57:54 +0530

    created Navbar using react icons and react avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "./components/shared/Navbar";
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
+      <Navbar />
+    </div>
+      
+    
   );
 }
 
