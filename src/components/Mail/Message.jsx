966ba01d49d4d671cdd393ca611929a29d726bdf
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-10-31 04:14:51 +0530

    Bug fix

--- src/components/Mail/Message.jsx
+++ src/components/Mail/Message.jsx
@@ -4,40 +4,50 @@ import { RiStarLine } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const Message = ({email, index}) => {
-    const selectedMailPath = useSelector((state) => state.navSlice.selectedMailPath);
-    const navigate = useNavigate();
-    const openMail = () => {
-        navigate(`/${selectedMailPath}/${email.id}`);
-    }
+const Message = ({ email, index }) => {
+    console.log(email);
+  const selectedMailPath = useSelector(
+    (state) => state.navSlice.selectedMailPath
+  );
+  const navigate = useNavigate();
 
-    const stripHtml = (html) => {
-        const div = document.createElement("div");
-        div.innerHTML = html;
-        return div.textContent || div.innerText || "";
-      };
+  const openMail = () => {
+    navigate(`/${selectedMailPath}/${email.id}`);
+  };
+
+  const stripHtml = (html) => {
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    return div.textContent || div.innerText || "";
+  };
 
   return (
-    <div onClick={openMail} style={{ '--delay': `${index * 400}ms` }} className="flex items-start justify-between border-b border-gray-200 px-4 py-2 text-sm cursor-pointer hover:bg-rose-300/20 hover:shadow-md hover:-translate-y-1 transition-all duration-300 ease-in-out animate-slideUp opacity-0 [animation-delay:var(--delay)]">
-        <div className="flex items-center gap-3">
-            <div className="flex-none text-gray-300">
-                <MdCropSquare className="w-5 h-5" /> 
-                {/* {improve} */}
-            </div>
-            <div className="flex-none text-gray-300">
-                <RiStarLine className="w-5 h-5" />
-            </div>
-            <div className="flex-none text-gray-300">
-                <MdLabelImportantOutline className="w-5 h-5" />
-            </div>
+    <div
+      onClick={openMail}
+      style={{ "--delay": `${index * 400}ms` }}
+      className="flex items-start justify-between border-b border-gray-200 px-4 py-2 text-sm cursor-pointer hover:bg-rose-300/20 hover:shadow-md hover:-translate-y-1 transition-all duration-300 ease-in-out animate-slideUp opacity-0 [animation-delay:var(--delay)]"
+    >
+      <div className="flex items-center gap-3">
+        <div className="flex-none text-gray-300">
+          <MdCropSquare className="w-5 h-5" />
+          {/* {improve} */}
         </div>
-        <div className="flex-1 ml-4 flex items-center">
-            <p className="text-black font-bold w-64">{email?.to}</p>
-            <p className="text-gray-600 truncate inline-block w-[50rem]"><strong>{email?.subject}</strong>-{stripHtml(email?.message)}</p>
+        <div className="flex-none text-gray-300">
+          <RiStarLine className="w-5 h-5" />
         </div>
-        <div className="flex-none text-gray-400 text-sm">
-            <p>{email?.createdAt}</p>
+        <div className="flex-none text-gray-300">
+          <MdLabelImportantOutline className="w-5 h-5" />
         </div>
+      </div>
+      <div className="flex-1 ml-4 flex items-center">
+        <p className="text-black font-bold w-64">{(selectedMailPath === "inbox" && email?.from) || (selectedMailPath === "sent" && email?.to)}</p>
+        <p className="text-gray-600 truncate inline-block w-[50rem]">
+          <strong>{email?.subject}</strong>-{stripHtml(email?.message)}
+        </p>
+      </div>
+      <div className="flex-none text-gray-400 text-sm">
+        <p>{email?.createdAt}</p>
+      </div>
     </div>
   );
 };
